34905e1c252b20c819bc05d61ea4d5f2cb3c0e4e
Roberto <w.parrav@uniandes.edu.co>
2023-11-18 21:45:19 -0300

    setting TypeORM and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocioModule } from './socio/socio.module';
 import { ClubModule } from './club/club.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocioEntity } from './socio/socio.entity';
+import { ClubEntity } from './club/club.entity';
 
 @Module({
-  imports: [SocioModule, ClubModule],
+  imports: [
+    SocioModule,
+    ClubModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'social_club',
+      entities: [SocioEntity, ClubEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
